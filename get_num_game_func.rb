acd54e1be0dc75960d33d0a255ab61a37a5fba9b
Don Kingery <kingery1@suddenlink.net>
2017-01-16 14:59:41 -0500

    third test

--- get_num_game_func.rb
+++ get_num_game_func.rb
@@ -6,8 +6,15 @@ def target_number
 
 puts target_number
 
-def compare_guess(guess, target_number)
-"Your guess was low"
+    def compare_guess(guess, target_number)
+        #"Your guess was low"
+        if guess < target_number
+            "Your guess was low"
+        elsif guess > target_number
+        "Your guess was high"
+        else 
+            "Your guess was correct"
+
+ end
+
 end
-
-
--- get_num_game_test.rb
+++ get_num_game_test.rb
@@ -11,10 +11,27 @@ require_relative "get_num_game_func.rb"
     def test_guess_true_or_false
         target_number=20
         guess=15
-        #assert_equal(("ur guess was low"), compare_guess, guess)
         assert_equal("Your guess was low", compare_guess(guess, target_number))
 
 	end
+
+def test_guess_true_or_false
+        target_number=20
+        guess=25
+        assert_equal("Your guess was high", compare_guess(guess, target_number))
+
+	end
+
+def test_guess_true_or_false
+        target_number=20
+        guess=20
+        assert_equal("Your guess was correct", compare_guess(guess, target_number))
+
+	end
+
+
+
+
 end
 
 
