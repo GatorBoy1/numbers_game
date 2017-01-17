04323fe03287878dba04f069ee83cfbe260d1e75
lyle Sanders <lylesanders66@yahoo.com>
2017-01-17 14:13:02 -0500

    second ERB requesting guess

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 # app.rb
 require 'sinatra'
 require_relative "get_num_game_func.rb"
+target_number
 
     get '/'do
         erb :get_name
     end
 
     post '/users_name' do
-        name = params[:user_name]
+        name = params[:user_name].capitalize
+        erb :get_number, :locals => {:name =>name}
     end
