0debe0d8b6b4a2ca0c65c4dd9269374dd7da60a9
lyle Sanders <lylesanders66@yahoo.com>
2017-01-17 10:43:50 -0500

    updating with first ERB file

--- app.rb
+++ app.rb
@@ -2,4 +2,10 @@
 require 'sinatra'
 require_relative "get_num_game_func.rb"
 
-get'/'do
+    get '/'do
+        erb :get_name
+    end
+
+    post '/users_name' do
+        name = params[:user_name]
+    end
